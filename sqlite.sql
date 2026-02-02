ad691dcb4e735d41e1c2cff6d56c49f0f00dd348
kihyeon yang <khyang@bizdata.kr>
2026-02-02 16:04:38 +0900

    초기 데이터 추가

--- sqlite.sql
+++ sqlite.sql
@@ -5,7 +5,8 @@
 */
 CREATE TABLE scrap_targets(
    seq INTEGER PRIMARY KEY AUTOINCREMENT,
-   type TEXT NOT NULL CHECK (type IN ('PAGE', 'API')),
+   type TEXT NOT NULL CHECK (type IN ('PAGE', 'API')) DEFAULT 'PAGE',
+   pagination_path TEXT,
    site_url TEXT NOT NULL,
    site_name TEXT,
    category TEXT,
@@ -28,48 +29,96 @@ CREATE TRIGGER trg_scrap_targets_updated
         UPDATE scrap_targets SET updated = CURRENT_TIMESTAMP WHERE seq = OLD.seq;
     END;
 
-INSERT INTO scrap_targets (type, site_url, site_name, category, detail_url_format, list_path, id_path, id_attr, id_regex, title_path)
+INSERT INTO scrap_targets (site_url, site_name, category, detail_url_format, list_path, id_path, id_attr, id_regex, title_path)
 VALUES (
-        'PAGE',
         'https://www.seoul.go.kr/news/news_notice.do?bbsNo=277&curPage={}',
         '서울특별시',
         '고시공고',
         'https://www.seoul.go.kr/news/news_notice.do?bbsNo=277&nttNo={}',
         '//*[@id="seoul-integrated-board"]/table/tbody/tr',
-        'td[2]/a',
+        '//td[2]/a',
         'href',
         'fnTbbsView\(''(\d+)''\)',
-        'td[2]/a'
+        '//td[2]/a'
        );
 
-INSERT INTO scrap_targets (type, site_url, site_name, category, detail_url_format, list_path, id_path, id_attr, id_regex, title_path)
+INSERT INTO scrap_targets (site_url, site_name, category, detail_url_format, list_path, id_path, id_attr, id_regex, title_path)
 VALUES (
-        'PAGE',
-        'https://www.busan.go.kr/nbgosi?curPage={}',
-        '부산광역시',
-        '고시공고',
-        'https://www.busan.go.kr/nbgosi/view?gosiGbn=A&sno={}',
-        '//*[@id="contents"]/div[2]/table/tbody/tr',
-        'td[2]/a',
-        'href',
-        'sno=(\d+)&',
-        'td[2]/a'
-       );
+    'https://www.busan.go.kr/nbgosi?curPage={}',
+    '부산광역시',
+    '고시공고',
+    'https://www.busan.go.kr/nbgosi/view?gosiGbn=A&sno={}',
+    '//*[@id="contents"]/div[2]/table/tbody/tr',
+    '//td[2]/a',
+    'href',
+    'sno=(\d+)&',
+    '//td[2]/a'
+);
 
-INSERT INTO scrap_targets (type, site_url, site_name, category, detail_url_format, list_path, id_path, id_attr, id_regex, title_path)
+INSERT INTO scrap_targets (site_url, site_name, category, detail_url_format, list_path, id_path, id_attr, id_regex, title_path)
 VALUES (
-        'PAGE',
-        'https://ulsan.go.kr/u/rep/transfer/notice/list.ulsan?mId=001004002000000000&curPage={}',
-        '울산광역시',
-        '고시공고',
-        'https://ulsan.go.kr/u/rep/transfer/notice/{}.ulsan?mId=001004002000000000&gosiGbn=A',
-        '//*[@id="contents_inner"]/div[3]/table/tbody/tr',
-        'td[2]/a',
-        'href',
-        '.\/(\d+).ulsan',
-        'td[2]/a'
-       );
+    'https://ulsan.go.kr/u/rep/transfer/notice/list.ulsan?mId=001004002000000000&srchGubun=A&srchType=srchSj&curPage={}',
+    '울산광역시',
+    '고시공고',
+    'https://ulsan.go.kr/u/rep/transfer/notice/{}.ulsan?mId=001004002000000000&gosiGbn=A',
+    '//*[@id="contents_inner"]/div[3]/table/tbody/tr',
+    '//td[2]/a',
+    'href',
+    '.\/(\d+).ulsan',
+    '//td[2]/a'
+);
 
+INSERT INTO scrap_targets (pagination_path, site_url, site_name, category, detail_url_format, list_path, id_path, id_attr, title_path)
+VALUES (
+    '//*[@id="app"]/div[4]/div/ul/li/a',
+    'https://www.jeju.go.kr/news/news/law/jeju2.htm',
+    '제주특별자치도',
+    '고시공고',
+    'https://www.jeju.go.kr/news/news/law/jeju2.htm{}',
+    '//*[@id="gosiBody"]/tr',
+    '//td[3]/a',
+    'href',
+    '//td[3]/a'
+);
+
+INSERT INTO scrap_targets (site_url, site_name, category, detail_url_format, list_path, id_path, id_attr, id_regex, title_path)
+VALUES (
+    'https://www.bizinfo.go.kr/sii/siia/selectSIIA200View.do?rows=15&cpage={}',
+    '기업마당',
+    '지원사업',
+    'https://www.bizinfo.go.kr/sii/siia/selectSIIA200Detail.do?schEndAt=N&pblancId={}',
+    '//*[@id="articleSearchForm"]/div[2]/div[4]/table/tbody/tr',
+    '//td[3]/a',
+    'href',
+    'pblancId=(PBLN_\d+)',
+    '//td[3]/a'
+);
+
+INSERT INTO scrap_targets (site_url, site_name, category, detail_url_format, list_path, id_path, id_attr, id_regex, title_path)
+VALUES (
+    'https://www.bizinfo.go.kr/sii/siia/selectSIIA200View.do?rows=15&cpage={}',
+    '기업마당',
+    '지원사업',
+    'https://www.bizinfo.go.kr/sii/siia/selectSIIA200Detail.do?schEndAt=N&pblancId={}',
+    '//*[@id="articleSearchForm"]/div[2]/div[4]/table/tbody/tr',
+    '//td[3]/a',
+    'href',
+    'pblancId=(PBLN_\d+)',
+    '//td[3]/a'
+);
+
+INSERT INTO scrap_targets (site_url, site_name, category, detail_url_format, list_path, id_path, id_attr, id_regex, title_path)
+VALUES (
+    'https://www.k-startup.go.kr/web/contents/bizpbanc-ongoing.do?page={}',
+    '창업넷',
+    '사업공고',
+    'https://www.k-startup.go.kr/web/contents/bizpbanc-ongoing.do?pbancClssCd=PBC010&schM=view&pbancSn={}',
+    '//*[@id="bizPbancList"]/ul/li',
+    '//div/div[1]/div[2]/a',
+    'href',
+    'go_view\((\d+)\)',
+    '//div/div[1]/div[2]/a'
+);
 
 /*
 
