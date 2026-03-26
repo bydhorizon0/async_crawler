bd00ca02f19fb1e3c07bab48414143373dd268c4
bydhorizon0 <bydhorizon0@gmail.com>
2026-03-26 23:31:34 +0900

    Context Queue, Page Queue, Semaphore

--- main_async.py
+++ main_async.py
@@ -2,45 +2,59 @@ import asyncio
 from asyncio import Queue
 from typing import Final
 
-from playwright.async_api import async_playwright
+from playwright.async_api import async_playwright, Page
 
 from core.database import DatabaseManager, ScrapTarget
 from scraper.engine import worker
 
 WORKERS: Final[int] = 4
+CONTEXT_POOL_SIZE: Final[int] = 3
+PAGES_PER_CONTEXT: Final[int] = 2
 
 
 async def main():
     db_mgr = DatabaseManager()
     await db_mgr.initialize()
-    targets = await db_mgr.select_scrap_targets("PAGE")
-    queue: Queue[ScrapTarget] = asyncio.Queue()
 
-    for t in targets:  # 수집 리스트
+    targets = await db_mgr.select_scrap_targets("PAGE")
+
+    queue: Queue[ScrapTarget | None] = asyncio.Queue()
+    page_queue: Queue[Page] = asyncio.Queue()
+
+    # 수집 목록 큐
+    for t in targets:
         queue.put_nowait(t)
 
-    # PAGE 수집
+    for _ in range(WORKERS):
+        queue.put_nowait(None)
+
     async with async_playwright() as p:
         browser = await p.chromium.launch(headless=True)
 
+        # Context Pool
+        contexts = [await browser.new_context() for _ in range(CONTEXT_POOL_SIZE)]
+        # Page Pool
+        pages: list[Page] = []
+
+        for context in contexts:
+            for _ in range(PAGES_PER_CONTEXT):
+                page = await context.new_page()
+                pages.append(page)
+                await page_queue.put(page)
+
         async with asyncio.TaskGroup() as tg:
-            workers = [
-                tg.create_task(worker(queue, browser, db_mgr), name=f"Worker-{i}")
-                for i in range(WORKERS)
-            ]
+            for i in range(WORKERS):
+                tg.create_task(worker(queue, page_queue, db_mgr), name=f"Worker-{i}")
 
             await queue.join()
 
-        await browser.close()
+        for page in pages:
+            await page.close()
 
-    # API 요청 수집
+        for context in contexts:
+            await context.close()
+        await browser.close()
 
 
 if __name__ == "__main__":
     asyncio.run(main())
-
-"""
-- Queue 를 사용해 동시성 제어, Semaphore 를 사용해도 되지만 Queue 가 더 효율적
-- 예외 추가
-- 로그 추가
-"""
--- scraper/engine.py
+++ scraper/engine.py
@@ -1,46 +1,52 @@
 import asyncio
-import random
 import re
 from asyncio import Queue
 
 import playwright
-from playwright.async_api import Browser, Page
+from playwright.async_api import Page
 
 from core.database import DatabaseManager
 from models.scrap_target import ScrapTarget
 
 
-async def worker(queue: Queue, browser: Browser, db_mgr: DatabaseManager):
-    page = await browser.new_page()
+semaphore = asyncio.Semaphore(3)
 
-    try:
-        while True:
-            try:
-                target: ScrapTarget = queue.get_nowait()
-            except asyncio.QueueEmpty:
-                break
 
-            # 예외가 발생해도 TaskGroup으로 예외가 전파되지 않고 수집을 계속 진행
-            try:
-                await scrape(page, target)
-                await asyncio.sleep(random.uniform(2.2, 5.7))
-            except (playwright.async_api.Error, Exception) as e:
-                print(f"[수집 실패] {target.site_name}({target.seq}): {e}")
+async def worker(
+    queue: Queue[ScrapTarget | None],
+    page_queue: Queue[Page],
+    db_mgr: DatabaseManager,
+) -> None:
+    while True:
+        target = await queue.get()
 
-                error_msg = str(e)
-                # await db_mgr.insert_scrap_error(target.seq, error_msg)
-            finally:
-                # queue.task_done()이 호출되지 않으면 queue.join()은 작업이 남은 줄 알고 계속 대기하게 된다.
-                # 실패 상황에서도 반드시 호출되어야 프로그램이 정상 종료된다.
-                queue.task_done()
-    finally:
-        await page.close()  # page 누수 방지를 위해 반드시 종료
+        if target is None:
+            queue.task_done()
+            break
+
+        page = await page_queue.get()
+
+        # 예외가 발생해도 TaskGroup으로 예외가 전파되지 않고 수집을 계속 진행
+        try:
+            await scrape(page, target)
+        except (playwright.async_api.Error, Exception) as e:
+            print(f"[수집 실패] {target.site_name}({target.seq}): {e}")
+
+            error_msg = str(e)
+            # await db_mgr.insert_scrap_error(target.seq, error_msg)
+        finally:
+            # queue.task_done()이 호출되지 않으면 queue.join()은 작업이 남은 줄 알고 계속 대기하게 된다.
+            # 실패 상황에서도 반드시 호출되어야 프로그램이 정상 종료된다.
+            await page_queue.put(page)  # 다시 반납
+            queue.task_done()
 
 
 async def scrape(page: Page, target: ScrapTarget):
     for i in range(1, 4):
         if target.pagination_path:
-            await page.goto(target.site_url)
+            async with semaphore:
+                await page.goto(target.site_url)
+
             await page.locator(target.pagination_path).nth(i).click()
         else:
             await page.goto(
@@ -72,6 +78,8 @@ async def scrape(page: Page, target: ScrapTarget):
 
             try:
                 detail_url = target.detail_url_format.format(*format_args)
-                print(f"{target.site_name}({target.seq}) 수집 {title.strip()}, {detail_url}")
+                print(
+                    f"{target.site_name}({target.seq}) 수집 {title.strip()}, {detail_url}"
+                )
             except IndexError:
                 print(f"[{target.site_name}] URL 포맷팅 실패: 인자 개수 불일치")
